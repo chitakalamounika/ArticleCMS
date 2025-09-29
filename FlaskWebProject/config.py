45482b8e05aba9a8e9451df227ca386288632793
chitakalamounika <93363612+chitakalamounika@users.noreply.github.com>
2025-09-29 07:33:55 +0530

    Update config.py

--- FlaskWebProject/config.py
+++ FlaskWebProject/config.py
@@ -1,6 +1,9 @@
 import os
 
+
 class Config:
+    """Configuration class for Flask CMS project."""
+
     # Secret key for session management
     SECRET_KEY = os.environ.get("SECRET_KEY", "supersecretkey")
 
@@ -10,14 +13,20 @@ class Config:
     # Azure AD / MSAL config
     CLIENT_ID = os.environ.get("CLIENT_ID", "your-client-id")
     CLIENT_SECRET = os.environ.get("CLIENT_SECRET", "your-client-secret")
-    AUTHORITY = os.environ.get("AUTHORITY", "https://login.microsoftonline.com/common")
+    AUTHORITY = os.environ.get(
+        "AUTHORITY",
+        "https://login.microsoftonline.com/common"
+    )
     REDIRECT_PATH = "/getAToken"
     SCOPE = ["User.Read"]
 
     # Azure Blob Storage config
     BLOB_CONNECTION_STRING = os.environ.get("BLOB_CONNECTION_STRING")
     BLOB_CONTAINER = os.environ.get("BLOB_CONTAINER", "cms")
-    BLOB_URL = os.environ.get("BLOB_URL")  # e.g. "https://<storageaccount>.blob.core.windows.net/article-images"
+    BLOB_URL = os.environ.get(
+        "BLOB_URL"
+    )  # Example: "https://<storage>.blob.core.windows.net/article-images"
+
 
 # Default config object
 config = Config()
